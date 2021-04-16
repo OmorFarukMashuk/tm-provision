58c0d5f5f25e2ae6e375b92d644236fbff6efc45
Giovanni Ferrenan <gferrenan@telmax.com>
2021-04-16 16:26:57 -0400

    first changes

--- internet/mcpstruct.go
+++ internet/mcpstruct.go
@@ -32,3 +32,79 @@ type MCPDeviceActivate struct {
 		DeviceName string `json:"device-name"`
 	}
 }
+
+
+
+
+type MCPInterfaceCreate struct {
+	InterfaceContext struct {
+		InterfaceName              string `json:"interface-name"`
+		InterfaceType               string `json:"interface-type"`
+	} `json:"device-context"`
+	ProfileVector     string `json:"profile-vector-name"`
+}
+
+type MCPInterfaceDeploy struct {
+	InterfaceContext struct {
+		InterfaceName       string `json:"interface-name"`
+		InterfaceType       string `json:"interface-type"`
+		DeviceName string `json:"device-name"`
+		InterfaceID       string `json:"interface-id"`
+	}
+}
+
+type MCPInterfaceActivate struct {
+	InterfaceContext struct {
+		InterfaceName string `json:"interface-name"`
+	}
+}
+
+
+
+
+
+
+
+type MCPServiceCreate struct {
+	ServiceContext struct {
+		ServiceID              string `json:"service-id"`
+		ServiceType            string `json:"service-type"`
+		RemoteID               string `json:"remote-id"`
+		CircuitID              string `json:"circuit-id"`
+	} `json:"device-context"`
+
+	ProfileName     string `json:"profile-name"`
+}
+
+type MCPServiceDeploy struct {
+	ServiceContext struct {
+		ServiceID       string `json:"service-name"`
+
+		UplinkContext struct {
+			InterfaceEndpoint struct {
+				OuterTagVlanID string `json:"outer-tag-vlan-id"`
+				InnerTagVlanID string `json:"inner-tag-vlan-id"`
+				ContentProviderName string `json:"content-provider-name"`
+			}
+		} `json:"uplink-context"`
+
+
+		DownlinkContext struct {
+			InterfaceEndpoint struct {
+				OuterTagVlanID string `json:"outer-tag-vlan-id"`
+				InnerTagVlanID string `json:"inner-tag-vlan-id"`
+				InterfaceName string `json:"interface-name"`
+			}
+		} `json:"downlink-context"`
+
+	}
+}
+
+type MCPServiceActivate struct {
+	ServiceContext struct {
+		ServiceID string `json:"service-id"`
+	}
+}
+
+
+
